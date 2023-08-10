c2717fba122008d5c8e8ee886b4f7d9164074b22
FrancoEMTG <aesevv@gmail.com>
2023-08-10 11:24:06 -0300

    modificando las rutas y en el entrypoint

--- CLASE-25-MICRODESAFIO/app.js
+++ CLASE-25-MICRODESAFIO/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require("path");
-const publicPath = path.resolve(__dirname, ("./public"))
 
+const publicPath = path.resolve(__dirname, ("./public"))
 app.use(express.static(publicPath))
+
+const viewsPath = path.resolve(__dirname,("./views"))
+app.use(express.static(viewsPath))
 
 app.listen(3000, () => {
     console.log("Servidor corriendo");
